9b45acf478c744e904c73f8d90279bbfdfa9d717
Samangula Rajender <144688006+Rajendersamangula@users.noreply.github.com>
2023-09-13 21:55:37 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/***while loop***/
+
+
+
+
+
+
+
+
+
+
+/*** infinite loop using while loop ***/
+//
+//#include <stdio.h>
+//void main()
+//{
+//    int i = 10;
+//    while (-75)
+//    {
+//        printf("%d",i);
+//        i++;
+//    }
+
+/*** do while loop***/
+
+/*** it is a exit controlled loop because it check condition is exiting ***/
+/*
+// FIBONACCI SERIES UPTO 20//
+// IN this series all numbers start with zero,//
+// 0 1 1 2 3 5  13//
+//  it will start with first and second number that is 0 and 1 //
+//  then sum of previous two numbers that is 0+1 = 1 //
+
+a= 0
+b = 1
+print (0 1 1 2)
+
+c = a+b
+print a
+a=b
+b=c
+a b c
+0 1 1 //print a  that is 0
+1 1 2 // print a that is  1
+1 2 3 // print  a that is 1
+2 3 5 // print  a that is  2
+
+
+do
+{
+}while (condition);
+
+*/
+//
+//int  a = 0,b = 1,c,n;
+//printf("Enter the limit:");
+//scanf("%d",&n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*** pointer***/
+
+
+//IIES Bangalore
+//8:27 PM
+/*
+pointer: Variable that stores address of another Variable
+int *x;
+x is a pointer that stores address of a variable whose data type is int
+int a;
+x=&a;
+only data(a) is available(a is data, &a is address)
+only address(x) is avaliable(*x is data, x is address)
+*/
+
+/*
+#include <stdio.h>
+void main()
+{
+   int a=90;//data
+   int *x;//address
+   x=&a;
+   printf("DATA A=%d",a);
+   printf("\nADDRESS OF DATA=%p",&a);
+   printf("\nDATA=%d",*x);
+   printf("\nADDRESS OF DATA=%p",x);
+}
+
+*/
+
+/*** pointer example***/
+
+
+/*
+#include <stdio.h>
+void main()
+{
+   int a[]={23,56,32,15,90};
+   int *x;
+   x=a;//base address will be stored in x(address of a[0] is stored in x)
+   printf("%d",*x);//*x will print value of a[0]
+   x++;//x is pointing a[1](x has the address of a[1])
+  printf("\n%d",*x);
+    x=x+3;
+     printf("\n%d",*x);
+}
+*/
+
+
